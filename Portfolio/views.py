a9c5d0a634969ffecf8fcce481ccac9fc9df0f8d
Sunil Patel <sunilpatelmca1810@gmail.com>
2014-01-27 15:27:49 +0530

    Initial revision

---
+++ Portfolio/views.py
@@ -0,0 +1,14 @@
+from django.http import HttpResponse
+from django.shortcuts import render_to_response
+import datetime
+
+def about(request):
+    #html = "<html><body>It is now %s.</body></html>" % now
+    template = "home.html"
+    #return HttpResponse(html)
+    return render_to_response(template)
+
+def profile(request):
+    now = datetime.datetime.now()
+    template = "profile.html"
+    return render_to_response(template, {'Time': now})
